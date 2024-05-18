4e5871dad60e695d8224f3354ec6db3e07ad8fc1
Felix <fdbatista@gmail.com>
2024-05-18 09:03:22 +0200

    #1 :wrench: Format dates in current timezone

--- src/utils/date.util.ts
+++ src/utils/date.util.ts
@@ -1,8 +1,11 @@
-import { format } from 'date-fns'
+import { formatInTimeZone } from 'date-fns-tz'
+const DEFAULT_TZ = 'Europe/Berlin'
 
 export class DateUtil {
     static formatCurrentDate() {
         const now: Date = new Date();
-        return format(now, 'yyyy-mm-dd HH:mi:ss');
+        const timezone = process.env.TZ ?? DEFAULT_TZ
+
+        return formatInTimeZone(now, timezone, 'yyyy-MM-dd HH:mm:ss');
     }
 }
