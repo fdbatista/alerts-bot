cd39c0dde0eebf0e98bcde2adb0abf3db6f9b5a2
Felix <fdbatista@gmail.com>
2024-06-13 07:42:13 +0200

    #1 :construction: Refactor patterns service

--- src/modules/technical-analysis/patterns.service.ts
+++ src/modules/technical-analysis/patterns.service.ts
@@ -19,51 +19,11 @@ export class PatternsService extends TechnicalAnalyzerAbstract {
         const closingPrices = await this.getClosingPrices(5);
 
         const peaks = this.findMaxPeaks(closingPrices);
-        const [lastPrice] = closingPrices.slice(-1);
+        const [currentPrice] = closingPrices.slice(-1);
 
-        return this.isPotentialBullishDivergence(peaks, lastPrice);
-    }
+        const nextPeak = this.calculateNextPeak(peaks);
 
-    private isPotentialBullishDivergence(peaks: number[], lastPrice: number): boolean {
-        const { length: peakCount } = peaks;
-
-        let result = false;
-
-        if (peakCount > 0 && this.isDescending(peaks)) {
-            const maxPeak = Math.max(...peaks);
-
-            result = lastPrice >= maxPeak;
-        }
-
-        return result
-    }
-
-    isPotentialShoulderHeadShoulder(peaks: number[], lastPrice: number): boolean {
-        const lastPeaks = peaks.slice(-3)
-        const { length: peakCount } = lastPeaks;
-
-        let result = false;
-
-        if (peakCount > 0) {
-            if (peakCount === 2) {
-                const minPeak = Math.min(...peaks);
-                result = lastPrice <= minPeak;
-            } else {
-                const [firstPeak, secondPeak, thirdPeak] = lastPeaks;
-                result = secondPeak > firstPeak && secondPeak > thirdPeak && lastPrice <= thirdPeak
-            }
-        }
-
-        return result
-    }
-
-    private isDescending(arr: number[]): boolean {
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] < arr[i + 1]) {
-                return false;
-            }
-        }
-        return true;
+        return currentPrice > nextPeak;
     }
 
     findMaxPeaks(prices: number[]): number[] {
@@ -80,5 +40,34 @@ export class PatternsService extends TechnicalAnalyzerAbstract {
         }
 
         return peaks;
+    }
+
+    isCurrentPriceOverLastPeak(peaks: number[], lastPrice: number): boolean {
+        const { length: peakCount } = peaks;
+
+        let result = false;
+
+        if (peakCount > 0 && this.isDescending(peaks)) {
+            const [lastPeak] = peaks.slice(-1);
+            result = lastPrice >= lastPeak;
+        }
+
+        return result
+    }
+
+    private isDescending(arr: number[]): boolean {
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] < arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private calculateNextPeak(peaks: number[]): number {
+        const [penultimatePeak, lastPeak] = peaks.slice(-2);
+        const peakSlope = (penultimatePeak - lastPeak)
+        
+        return lastPeak - peakSlope;
     }
 }
