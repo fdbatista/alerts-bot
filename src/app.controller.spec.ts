c3672f1d8d69a37a41f2880f9b057a4f2d99eba4
Felix <fdbatista@gmail.com>
2024-06-13 07:41:08 +0200

    #1 ::white_check_mark:: Fix app.controller tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvModule } from './modules/_common/env/env.module';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -9,13 +10,14 @@ describe('AppController', () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
+      imports: [EnvModule],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return a valid response', () => {
       expect(appController.getDefault()).toBeTruthy();
     });
   });
