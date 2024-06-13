e49b3a788f2f913c2fe05ceeada1b4237d55639d
Felix <fdbatista@gmail.com>
2024-06-13 23:31:51 +0200

    #1 ::white_check_mark:: Fix Bitso controller tests

--- src/modules/exchange/bitso/bitso.controller.spec.ts
+++ src/modules/exchange/bitso/bitso.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BitsoController } from './bitso.controller';
+import { BitsoService } from './bitso.service';
+import { HttpModule } from 'src/modules/_common/http/http.module';
+import { EnvModule } from 'src/modules/_common/env/env.module';
 
 describe('BitsoController', () => {
   let controller: BitsoController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, EnvModule],
       controllers: [BitsoController],
+      providers: [BitsoService],
     }).compile();
 
     controller = module.get<BitsoController>(BitsoController);
