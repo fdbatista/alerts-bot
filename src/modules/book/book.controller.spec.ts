840d849042055e07b692f93c40b41e681f59c435
Felix <fdbatista@gmail.com>
2024-06-13 23:29:25 +0200

    #1 ::white_check_mark:: Fix book controller tests

--- src/modules/book/book.controller.spec.ts
+++ src/modules/book/book.controller.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookController } from './book.controller';
+import { BookService } from './book.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Book } from 'src/database/entities/book';
+import { BitsoModule } from '../exchange/bitso/bitso.module';
 
 describe('BookController', () => {
   let controller: BookController;
 
   beforeEach(async () => {
+    const mockedBookRepository = {
+      find: jest.fn(() => {
+        return [];
+      }),
+      findOne: jest.fn(),
+      save: jest.fn(),
+      remove: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
+      imports: [BitsoModule],
       controllers: [BookController],
+      providers: [
+        BookService,
+        {
+          provide: getRepositoryToken(Book),
+          useValue: mockedBookRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<BookController>(BookController);
