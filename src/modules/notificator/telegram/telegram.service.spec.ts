b44fdb77da7db9edbe7e96a363e5808874297c8a
Felix <fdbatista@gmail.com>
2024-06-13 08:38:34 +0200

    #1 ::white_check_mark:: Fix telegram service tests

--- src/modules/notificator/telegram/telegram.service.spec.ts
+++ src/modules/notificator/telegram/telegram.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TelegramService } from './telegram.service';
+import { HttpModule } from 'src/modules/_common/http/http.module';
 
 describe('TelegramService', () => {
   let service: TelegramService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [TelegramService],
     }).compile();
 
