4c3e51c3ce4a38d467535f23957e0d650eece01f
Felix <fdbatista@gmail.com>
2024-06-13 08:38:41 +0200

    #1 ::white_check_mark:: Fix ticker service tests

--- src/modules/ticker/ticker.service.spec.ts
+++ src/modules/ticker/ticker.service.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TickerService } from './ticker.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Ticker } from 'src/database/entities/ticker';
+import { BitsoModule } from '../exchange/bitso/bitso.module';
 
 describe('TickerService', () => {
   let service: TickerService;
 
   beforeEach(async () => {
+    const mockedTickerRepository = {
+      find: jest.fn(() => {
+        return [];
+      }),
+      findOne: jest.fn(),
+      save: jest.fn(),
+      remove: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TickerService],
+      imports: [BitsoModule],
+      providers: [
+        TickerService,
+        {
+          provide: getRepositoryToken(Ticker),
+          useValue: mockedTickerRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<TickerService>(TickerService);
