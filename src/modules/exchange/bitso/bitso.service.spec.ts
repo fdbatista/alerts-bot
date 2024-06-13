a595e82a0d624f94969aea76d98ba3e57f0b2111
Felix <fdbatista@gmail.com>
2024-06-13 23:36:11 +0200

    #1 ::white_check_mark:: Fix Bitso service tests

--- src/modules/exchange/bitso/bitso.service.spec.ts
+++ src/modules/exchange/bitso/bitso.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BitsoService } from './bitso.service';
+import { HttpModule } from 'src/modules/_common/http/http.module';
+import { EnvModule } from 'src/modules/_common/env/env.module';
 
 describe('BitsoService', () => {
   let service: BitsoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, EnvModule],
       providers: [BitsoService],
     }).compile();
 
