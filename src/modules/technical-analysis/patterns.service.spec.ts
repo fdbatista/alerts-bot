fd8023724f455e4e83db3687a15f29947fa7a383
Felix <fdbatista@gmail.com>
2024-06-13 08:52:39 +0200

    #1 ::white_check_mark:: Fix patterns service tests

--- src/modules/technical-analysis/patterns.service.spec.ts
+++ src/modules/technical-analysis/patterns.service.spec.ts
@@ -10,22 +10,22 @@ describe('PatternsService', () => {
     const mockedTickerRepository = {
       find: jest.fn(() => {
         return [
-          { timestamp: '2024-05-20 23:55:00', last: 66980 },
-          { timestamp: '2024-05-20 23:54:00', last: 67113 },
-          { timestamp: '2024-05-20 23:53:00', last: 67110 },
-          { timestamp: '2024-05-20 23:52:00', last: 67105 },
-          { timestamp: '2024-05-20 23:51:00', last: 67123 },
-          { timestamp: '2024-05-20 23:50:00', last: 67077 },
-          { timestamp: '2024-05-20 23:49:00', last: 67077 },
-          { timestamp: '2024-05-20 23:48:00', last: 67090 },
-          { timestamp: '2024-05-20 23:47:00', last: 67100 },
-          { timestamp: '2024-05-20 23:46:00', last: 67020 },
-          { timestamp: '2024-05-20 23:45:00', last: 67030 },
-          { timestamp: '2024-05-20 23:44:00', last: 67040 },
-          { timestamp: '2024-05-20 23:43:00', last: 67010 },
-          { timestamp: '2024-05-20 23:42:00', last: 66990 },
-          { timestamp: '2024-05-20 23:41:00', last: 67115 },
-          { timestamp: '2024-05-20 23:40:00', last: 67114 },
+          { timestamp: new Date('2024-05-20 23:55:00'), last: 66980 },
+          { timestamp: new Date('2024-05-20 23:54:00'), last: 67113 },
+          { timestamp: new Date('2024-05-20 23:53:00'), last: 67110 },
+          { timestamp: new Date('2024-05-20 23:52:00'), last: 67105 },
+          { timestamp: new Date('2024-05-20 23:51:00'), last: 67123 },
+          { timestamp: new Date('2024-05-20 23:50:00'), last: 67077 },
+          { timestamp: new Date('2024-05-20 23:49:00'), last: 67077 },
+          { timestamp: new Date('2024-05-20 23:48:00'), last: 67090 },
+          { timestamp: new Date('2024-05-20 23:47:00'), last: 67100 },
+          { timestamp: new Date('2024-05-20 23:46:00'), last: 67020 },
+          { timestamp: new Date('2024-05-20 23:45:00'), last: 67030 },
+          { timestamp: new Date('2024-05-20 23:44:00'), last: 67040 },
+          { timestamp: new Date('2024-05-20 23:43:00'), last: 67010 },
+          { timestamp: new Date('2024-05-20 23:42:00'), last: 66990 },
+          { timestamp: new Date('2024-05-20 23:41:00'), last: 67115 },
+          { timestamp: new Date('2024-05-20 23:40:00'), last: 67114 },
         ];
       }),
       findOne: jest.fn(),
@@ -61,10 +61,9 @@ describe('PatternsService', () => {
     expect(result).toEqual(expectedResult);
   });
 
-  it('Should detect potential bullish divergence', async () => {
+  it('Should detect potential breakage', async () => {
     const result = await service.isPotentialBreakage();
-    const expectedResult = { bullish: false, bearish: true };
 
-    expect(result).toEqual(expectedResult);
+    expect(result).toEqual(false);
   });
 });
