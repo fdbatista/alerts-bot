ea32a7a921fff4aed022d6e1df17643edf0a7baa
Felix <fdbatista@gmail.com>
2024-06-13 08:27:44 +0200

    #1 ::white_check_mark:: Fix book service tests

--- src/modules/book/book.service.spec.ts
+++ src/modules/book/book.service.spec.ts
@@ -1,12 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookService } from './book.service';
+import { BitsoModule } from '../exchange/bitso/bitso.module';
+import { EnvModule } from '../_common/env/env.module';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import { BookController } from './book.controller';
+import { Book } from 'src/database/entities/book';
 
 describe('BookService', () => {
   let service: BookService;
 
   beforeEach(async () => {
+    const mockedBookRepository = {
+      find: jest.fn(() => {
+        return [];
+      }),
+      findOne: jest.fn(),
+      save: jest.fn(),
+      remove: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BookService],
+      imports: [EnvModule, BitsoModule],
+      providers: [
+        BookService,
+        {
+          provide: getRepositoryToken(Book),
+          useValue: mockedBookRepository
+        },
+      ],
+      controllers: [BookController],
     }).compile();
 
     service = module.get<BookService>(BookService);
