098a567b6ee833a837dbf13714c57687cd5feaa8
Felix <fdbatista@gmail.com>
2024-06-13 08:38:27 +0200

    #1 ::white_check_mark:: Fix notificator service tests

--- src/modules/notificator/notificator.service.spec.ts
+++ src/modules/notificator/notificator.service.spec.ts
@@ -1,12 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificatorService } from './notificator.service';
+import { TelegramService } from './telegram/telegram.service';
+import { EntrypointDetectorService } from '../technical-analysis/entrypoint-detector.service';
+import { HttpModule } from '../_common/http/http.module';
+import { IndicatorsService } from '../technical-analysis/indicators.service';
+import { PatternsService } from '../technical-analysis/patterns.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Ticker } from 'src/database/entities/ticker';
 
 describe('NotificatorService', () => {
   let service: NotificatorService;
 
   beforeEach(async () => {
+    const mockedTickerRepository = {
+      find: jest.fn(() => {
+        return [];
+      }),
+      findOne: jest.fn(),
+      save: jest.fn(),
+      remove: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotificatorService],
+      imports: [
+        HttpModule,
+      ],
+      providers: [
+        NotificatorService,
+        TelegramService,
+        EntrypointDetectorService,
+        IndicatorsService,
+        PatternsService,
+        {
+          provide: getRepositoryToken(Ticker),
+          useValue: mockedTickerRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<NotificatorService>(NotificatorService);
--- src/modules/notificator/notificator.service.ts
+++ src/modules/notificator/notificator.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import { LoggerUtil } from '../../utils/logger.util';
+import { LoggerUtil } from 'src/utils/logger.util';
 import { TelegramService } from './telegram/telegram.service';
 import { POTENTIAL_ENTRYPOINT_MESSAGE } from './_config';
-import { EntrypointDetectorService } from '../technical-analysis/entrypoint-detector.service';
+import { EntrypointDetectorService } from 'src/modules/technical-analysis/entrypoint-detector.service';
 
 @Injectable()
 export class NotificatorService {
