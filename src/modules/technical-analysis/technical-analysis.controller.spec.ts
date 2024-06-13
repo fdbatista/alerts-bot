d55d8f825acedfc717fc87bfb79cc74bbf027070
Felix <fdbatista@gmail.com>
2024-06-13 08:46:51 +0200

    #1 ::white_check_mark:: Fix tecnical analysis controller tests

--- src/modules/technical-analysis/technical-analysis.controller.spec.ts
+++ src/modules/technical-analysis/technical-analysis.controller.spec.ts
@@ -1,12 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TechnicalAnalysisController } from './technical-analysis.controller';
+import { IndicatorsService } from './indicators.service';
+import { EntrypointDetectorService } from './entrypoint-detector.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Ticker } from 'src/database/entities/ticker';
+import { PatternsService } from './patterns.service';
 
-describe('IndicatorController', () => {
+describe('TechnicalAnalysisController', () => {
   let controller: TechnicalAnalysisController;
 
   beforeEach(async () => {
+    const mockedTickerRepository = {
+      find: jest.fn(() => {
+        return [];
+      }),
+      findOne: jest.fn(),
+      save: jest.fn(),
+      remove: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TechnicalAnalysisController],
+      providers: [
+        IndicatorsService,
+        EntrypointDetectorService,
+        PatternsService,
+        {
+          provide: getRepositoryToken(Ticker),
+          useValue: mockedTickerRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<TechnicalAnalysisController>(TechnicalAnalysisController);
