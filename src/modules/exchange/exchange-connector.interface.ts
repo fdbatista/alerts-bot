5940e1fdc4693849175596ae567eeda634697d0f
Felix <fdbatista@gmail.com>
2024-06-30 16:26:28 +0200

    #2 :fire: Remove unused `book` stuff

--- src/modules/exchange/exchange-connector.interface.ts
+++
@@ -1,7 +0,0 @@
-import { BookDTO } from "../_common/dto/book-dto"
-import { TickerDTO } from "../_common/dto/ticker-dto"
-
-export interface IExchangeConnector {
-    getTicker(symbol: string): Promise<TickerDTO>
-    getBooks(): Promise<BookDTO[]>
-}
--- src/modules/exchange/exchange.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BitsoModule } from './bitso/bitso.module';
-
-@Module({
-  imports: [BitsoModule],
-  exports: [BitsoModule],
-})
-export class ExchangeModule {}
