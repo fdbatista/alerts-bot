45847daade10657ea51df6c15914e5a4d8515a89
Felix <fdbatista@gmail.com>
2024-07-15 14:51:10 +0200

    #2 :construction: Uncomment code in `technical-analysis.controller`

--- src/modules/technical-analysis/technical-analysis.controller.ts
+++ src/modules/technical-analysis/technical-analysis.controller.ts
@@ -1,24 +1,15 @@
 import { Controller, Get, Version } from '@nestjs/common';
-import { IndicatorsService } from './indicators.service';
 import { EntrypointDetectorService } from './entrypoint-detector.service';
 
 @Controller('technical-analysis')
 export class TechnicalAnalysisController {
     constructor(
-        private readonly indicatorService: IndicatorsService,
         private readonly entrypointDetectorService: EntrypointDetectorService,
     ) { }
 
-    // @Version('1')
-    // @Get('rsi')
-    // getRSI(candlestickDuration: number): Promise<number> {
-    //     // return this.indicatorService.rsi(candlestickDuration);
-    //     return candlestickDuration;
-    // }
-
-    // @Version('1')
-    // @Get('detection/entrypoint')
-    // async getPotentialEntrypoint(): Promise<object> {
-    //     return await this.entrypointDetectorService.isPotentialGoodEntrypoint();
-    // }
+    @Version('1')
+    @Get('detection/entrypoint')
+    async getPotentialEntrypoint(): Promise<object> {
+        return await this.entrypointDetectorService.isPotentialGoodEntrypoint();
+    }
 }
