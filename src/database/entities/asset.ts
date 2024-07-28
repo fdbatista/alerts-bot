10df2f40567139e642ef1f7e9301ba778f620bbf
Felix <fdbatista@gmail.com>
2024-07-28 15:41:17 +0200

    #1 Add `ema` table and update models

--- src/database/entities/asset.ts
+++ src/database/entities/asset.ts
@@ -8,11 +8,13 @@ import {
   PrimaryGeneratedColumn,
 } from "typeorm";
 import { AssetType } from "./asset-type";
+import { Ema } from "./ema";
 import { Rsi } from "./rsi";
 import { Stoch } from "./stoch";
 import { StrategySignal } from "./strategy-signal";
 import { Ticker } from "./ticker";
 
+@Index("asset_external_id_key", ["externalId"], { unique: true })
 @Index("UQ_119b2d1c1bdccc42057c303c44f", ["name"], { unique: true })
 @Index("UQ_45b83954906fc214e750ba53286", ["symbol"], { unique: true })
 @Entity("asset", { schema: "public" })
@@ -20,7 +22,7 @@ export class Asset {
   @PrimaryGeneratedColumn({ type: "integer", name: "id" })
   id: number;
 
-  @Column("character varying", { name: "external_id" })
+  @Column("character varying", { name: "external_id", unique: true })
   externalId: string;
 
   @Column("character varying", { name: "symbol", unique: true })
@@ -39,6 +41,9 @@ export class Asset {
   })
   @JoinColumn([{ name: "type_id", referencedColumnName: "id" }])
   type: Promise<AssetType>;
+
+  @OneToMany(() => Ema, (ema) => ema.asset, { lazy: true })
+  emas: Promise<Ema[]>;
 
   @OneToMany(() => Rsi, (rsi) => rsi.asset, { lazy: true })
   rsis: Promise<Rsi[]>;
