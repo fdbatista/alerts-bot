0ed34227a06f4db1c0545743f5b8da3afbec19a7
Felix <fdbatista@gmail.com>
2024-08-12 17:25:29 +0200

    #2 Update tickers loading schedule

--- src/modules/ticker/ticker-scheduler.service.ts
+++ src/modules/ticker/ticker-scheduler.service.ts
@@ -8,33 +8,23 @@ export class TickerSchedulerService {
     private readonly tickerIngesterService: TickerIngesterService,
   ) { }
   
-  @Cron(`30-59 15 * * 1-5`) // Every minute from 15:30 to 15:59 on Monday to Friday
-  async loadAllAssetsTickers1() {
-    this.tickerIngesterService.loadAllAssetsTickers()
-  }
-
-  @Cron(`* 16-21 * * 1-5`)  // Every minute from 16:00 to 21:59 on Monday to Friday
-  async loadAllAssetsTickers2() {
-    this.tickerIngesterService.loadAllAssetsTickers()
-  }
-  
-  @Cron(`* 0-14 * * 1-5`) // Every minute from 00:00 to 14:59 on Monday to Friday
-  async loadCryptoTickers1() {
+  @Cron(`* 0-9 * * 1-5`)  // Every minute from 00:00 to 09:59 on Monday to Friday
+  async loadCryptosFrom00To09() {
     this.tickerIngesterService.loadCryptoTickers()
   }
 
-  @Cron(`0-29 15 * * 1-5`)  // Every minute from 15:00 to 15:29 on Monday to Friday
-  async loadCryptoTickers2() {
-    this.tickerIngesterService.loadCryptoTickers()
+  @Cron(`* 10-21 * * 1-5`) // Every minute from 10:00 to 21:59 on Monday to Friday
+  async loadAllAssetsFrom10To21() {
+    this.tickerIngesterService.loadAllAssetsTickers()
   }
 
   @Cron(`* 22-23 * * 1-5`)  // Every minute from 22:00 to 23:59 on Monday to Friday
-  async loadCryptoTickers3() {
+  async loadCryptosFrom22To23() {
     this.tickerIngesterService.loadCryptoTickers()
   }
 
   @Cron(`* * * * 6,7`)  // Every minute on Saturday and Sunday
-  async loadCryptoTickers4() {
+  async loadCryptosOnWeekends() {
     this.tickerIngesterService.loadCryptoTickers()
   }
 
