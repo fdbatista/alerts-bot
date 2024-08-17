4a1d42fb1dd381dd3835ca8c8a76239973a237d8
Felix <fdbatista@gmail.com>
2024-08-17 10:32:00 +0200

    #2 Fix `ticker.controller` tests

--- src/modules/ticker/ticker.controller.spec.ts
+++ src/modules/ticker/ticker.controller.spec.ts
@@ -1,11 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TickerController } from './ticker.controller';
+import { TickerService } from './ticker.service';
+import { TickerRepository } from './ticker.repository';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Ticker } from 'src/database/entities/ticker';
+
+const mockRepository = () => {
+  return {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    remove: jest.fn(),
+  };
+}
 
 describe('TickerController', () => {
   let controller: TickerController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: getRepositoryToken(Ticker),
+          useValue: mockRepository(),
+        },
+        TickerService,
+        TickerRepository
+      ],
       controllers: [TickerController],
     }).compile();
 
