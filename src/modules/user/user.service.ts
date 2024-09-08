9beabda0bef7adbeca42d5387312fd0c43d96348
Felix <fdbatista@gmail.com>
2024-09-08 10:35:53 +0200

    :construction: Implement login feature

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcryptjs';
 import { UserRepository } from './user.repository';
 import { User } from 'src/database/entities/user';
 
@@ -8,12 +7,11 @@ export class UserService {
 
     constructor(private readonly userRepository: UserRepository) { }
 
-    async findOne(username: string): Promise<User | null> {
-        return this.userRepository.findByUsername(username);
-    }
+    async findByUsername(username: string): Promise<User> {
+        let result = await this.userRepository.findByUsername(username);
+        result = result ?? new User();
 
-    async validatePassword(plainPassword: string, hashedPassword: string): Promise<boolean> {
-        return bcrypt.compare(plainPassword, hashedPassword);
+        return result;
     }
 
 }
