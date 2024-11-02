7dcca1e499f62c23607e20184a2dd6c10a0f3f48
FD <fdbatista@gmail.com>
2024-11-02 22:49:22 +0100

    :construction: optimize loops

--- src/modules/technical-analysis/indicators-builder/indicator-calculator.service.ts
+++ src/modules/technical-analysis/indicators-builder/indicator-calculator.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import * as _ from 'lodash';
-
 import { Ticker } from 'src/database/entities/ticker';
-import { rsi, simpleMovingAverage, stoch, ema } from 'indicatorts';
+import { rsi, stoch, ema, sma } from 'indicatorts';
+import { CandlestickDTO } from 'src/modules/_common/dto/ticker-dto';
 
 const CANDLESTICKS_QUERY = `
     SELECT 
@@ -32,22 +31,20 @@ export class IndicatorService {
 
     for (const bucket of intervals) {
       const candlesticks = await this.generateCandlesticks(assetId, bucket, 200);
-      const closingsAsc = candlesticks.map(c => c.close).reverse();
-      const highsAsc = candlesticks.map(c => c.high).reverse();
-      const lowsAsc = candlesticks.map(c => c.low).reverse();
-      
-      const rsiResult = rsi(closingsAsc, { period: 14 });
-      const stockResult = stoch(highsAsc, lowsAsc, closingsAsc, { dPeriod: 14, kPeriod: 3 });
-      const emaResult = ema(closingsAsc, { period: 45 });
+      const { highs, lows, closings } = this.orderHighsLowsAndClosingsByTimeDesc(candlesticks);
+
+      const rsiResult = rsi(closings, { period: 14 });
+      const stockResult = stoch(highs, lows, closings, { dPeriod: 14, kPeriod: 3 });
+      const emaResult = ema(closings, { period: 45 });
 
       const smaResult = [];
 
       for (const length of smaLengths) {
-        const sma2 = simpleMovingAverage(closingsAsc, { period: length })
+        const smaLengthResult = sma(closings, { period: length })
 
         smaResult.push({
           length,
-          sma: sma2.at(-1),
+          sma: smaLengthResult.at(-1),
         });
       }
 
@@ -66,79 +63,29 @@ export class IndicatorService {
     return results;
   }
 
-  async generateCandlesticks(assetId: number, interval: number, length: number): Promise<any[]> {
+  async generateCandlesticks(assetId: number, interval: number, length: number): Promise<CandlestickDTO[]> {
     const query = CANDLESTICKS_QUERY
-      .replace(/:minutes/g, interval.toString())
-      .replace(/:length/g, `${interval * length}`);
+      .replaceAll(':minutes', interval.toString())
+      .replaceAll(':length', `${interval * length}`);
 
     return this.stockPriceRepository.query(query, [assetId]);
   }
 
-  async calculateStoch(assetId: number, length: number, interval: number): Promise<number> {
-    const query = `
-      WITH recent_prices AS (
-        SELECT price
-        FROM ticker
-        WHERE asset_id = $1
-          AND timestamp > NOW() - INTERVAL $2
-        ORDER BY timestamp DESC
-        LIMIT $3
-      ),
-      min_price AS (
-        SELECT MIN(price) AS min_price
-        FROM recent_prices
-      ),
-      max_price AS (
-        SELECT MAX(price) AS max_price
-        FROM recent_prices
-      )
-      SELECT
-        100 * (price - min_price.min_price) / NULLIF(max_price.max_price - min_price.min_price, 0) AS stochastic
-      FROM recent_prices, min_price, max_price
-      ORDER BY timestamp DESC
-      LIMIT 1;
-    `;
-    const result = await this.stockPriceRepository.query(query, [assetId, interval, length]);
-    return result[0]?.stochastic ?? 0;
-  }
+  private orderHighsLowsAndClosingsByTimeDesc(candlesticks: CandlestickDTO[]) {
+    const closings: number[] = [];
+    const highs: number[] = [];
+    const lows: number[] = [];
 
-  async calculateEMA(assetId: number, length: number, interval: number): Promise<number> {
-    const smoothingFactor = 2 / (length + 1);
+    const lastIndex = candlesticks.length - 1;
 
-    const smaQuery = `
-      SELECT AVG(price) AS sma
-      FROM (
-        SELECT price
-        FROM ticker
-        WHERE asset_id = $1
-          AND timestamp > NOW() - INTERVAL $2
-        ORDER BY timestamp DESC
-        LIMIT $3
-      ) AS initial_sma;
-    `;
+    for (let i = lastIndex; i >= 0; i--) {
+      const ticker = candlesticks[i];
 
-    const smaResult = await this.stockPriceRepository.query(smaQuery, [assetId, interval, length]);
-    const initialEMA = smaResult[0]?.sma;
-
-    if (initialEMA === undefined) return 0;
-
-    const emaQuery = `
-      SELECT price,
-             LAG(price) OVER (ORDER BY timestamp DESC) AS prev_price
-      FROM ticker
-      WHERE asset_id = $1
-        AND timestamp > NOW() - INTERVAL $2
-      ORDER BY timestamp DESC
-      LIMIT $3;
-    `;
-
-    const prices = await this.stockPriceRepository.query(emaQuery, [assetId, interval, length]);
-
-    let ema = initialEMA;
-    for (const { price } of prices) {
-      ema = (price * smoothingFactor) + (ema * (1 - smoothingFactor));
+      highs.push(ticker.high);
+      lows.push(ticker.low);
+      closings.push(ticker.close);
     }
 
-    return ema;
+    return { highs, lows, closings };
   }
 }
